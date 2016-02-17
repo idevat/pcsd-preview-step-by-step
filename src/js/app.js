5108251fa58f080c39faa80ecc28917c0417c981
Ivan Devat <idevat@redhat.com>
2016-02-17 20:48:12 +0100

    add domready

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 /*global document*/
 import React from 'react'
 import ReactDOM from 'react-dom'
+import domready from 'domready'
 
 let HelloWorld = () => <h1>Pcsd preview: step by step</h1>
 
-ReactDOM.render(
+domready(() => ReactDOM.render(
   <HelloWorld />,
   document.getElementById('app')
-);
+))
