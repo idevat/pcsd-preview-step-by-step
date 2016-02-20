66d13bafd020ba22769c71692b2bf15ceab248ff
Ivan Devat <idevat@redhat.com>
2016-02-20 18:38:53 +0100

    use class for ClusterAdd

--- src/js/components/ClusterAdd.js
+++ src/js/components/ClusterAdd.js
@@ -1,19 +1,24 @@
 import React, {PropTypes} from 'react'
 import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 
-let ClusterAdd = ({onClose}) =>
-<ModalContainer onClose={() => onClose()}>
-  <ModalDialog onClose={() => onClose()}>
-    <h1>Add cluster</h1>
-    <p>
-      Fill cluster info...
-    </p>
-    <button onClick={() => onClose()}>Ok</button>
-  </ModalDialog>
-</ModalContainer>
+class ClusterAdd extends React.Component{
+  static propTypes = {
+    onClose: PropTypes.func.isRequired,
+  }
 
-ClusterAdd.propTypes = {
-  onClose: PropTypes.func.isRequired,
+  render(){
+    let {onClose} = this.props
+    return <ModalContainer onClose={() => onClose()}>
+      <ModalDialog onClose={() => onClose()}>
+        <h1>Add cluster</h1>
+        <p>
+          Fill cluster info...
+        </p>
+        <button onClick={() => onClose()}>Ok</button>
+      </ModalDialog>
+    </ModalContainer>
+  }
 }
+
 
 export default ClusterAdd;
